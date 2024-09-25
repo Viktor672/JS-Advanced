2b2c0335de639ff37b432c008ed95eda88147e42
Viktor <149080919+Viktor672@users.noreply.github.com>
2024-09-26 00:52:49 +0300

    Delete DOM Manipulation and Events/demo.js

--- DOM Manipulation and Events/demo.js
+++
@@ -1,9 +0,0 @@
-let arr=[1,2,1,4,5];
-let uniqueProductArr = arr.reduce((acc, value) => {
-    if (!(acc.includes(value))) {
-       acc.push(value);
-    }
-    return acc;
- }, []);
- console.log(uniqueProductArr);
- 
